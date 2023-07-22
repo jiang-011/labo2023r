2d04f03f9f7c8e6db0770730d8ed0ed35823a4b6
jiang-011 <jiangwml2013@workflow-sem2.us-west4-a.c.soy-enterprise-389222.internal>
2023-07-22 13:19:51 +0000

    exp_sem2

--- src/workflow-semillerio/e771_ZZ_final_semillerio.r
+++ src/workflow-semillerio/e771_ZZ_final_semillerio.r
@@ -20,8 +20,8 @@ require("lightgbm")
 
 # Parametros del script
 PARAM <- list()
-PARAM$experimento <- "AA7710"
-PARAM$exp_input <- "AA7510"
+PARAM$experimento <- "AB7710"
+PARAM$exp_input <- "AB7510"
 
 # Que modelos quiero, segun su posicion en el ranking
 # de la Bayesian Optimizacion, ordenado por ganancia descendente
