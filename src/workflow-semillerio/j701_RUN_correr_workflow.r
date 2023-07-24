5ad472ecf7a93670fb17ca8db3ac3bfe44d93075
jiang-011 <jwmamanilopez@mail.austral.edu.ar>
2023-07-24 20:10:30 -0300

    exp semi2 alternativo 702337

--- src/workflow-semillerio/j701_RUN_correr_workflow.r
+++ src/workflow-semillerio/j701_RUN_correr_workflow.r
@@ -8,9 +8,6 @@ options(error = function() {
 
 
 # corrida de cada paso del workflow
-source("~/labo2023r/src/workflow-semillerio/i711_CA_reparar_dataset.r")
-source("~/labo2023r/src/workflow-semillerio/i721_DR_corregir_drifting.r")
-source("~/labo2023r/src/workflow-semillerio/i731_FE_historia.r")
-source("~/labo2023r/src/workflow-semillerio/i741_TS_training_strategy.r")
-source("~/labo2023r/src/workflow-semillerio/i751_HT_lightgbm.r")
-source("~/labo2023r/src/workflow-semillerio/i771_ZZ_final_semillerio.r")
+source("~/labo2023r/src/workflow-semillerio/j741_TS_training_strategy.r")
+source("~/labo2023r/src/workflow-semillerio/j751_HT_lightgbm.r")
+source("~/labo2023r/src/workflow-semillerio/j771_ZZ_final_semillerio.r")
