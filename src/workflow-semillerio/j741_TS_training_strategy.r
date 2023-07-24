29a1bbed58d324486784345e799d9e4e986f8ec0
jiang-011 <jwmamanilopez@mail.austral.edu.ar>
2023-07-24 20:12:41 -0300

    exp semi2 alternativo 702337

--- src/workflow-semillerio/j741_TS_training_strategy.r
+++ src/workflow-semillerio/j741_TS_training_strategy.r
@@ -13,7 +13,7 @@ require("yaml")
 PARAM <- list()
 PARAM$experimento <- "AJ7410"
 
-PARAM$exp_input <- "AJ7310"
+PARAM$exp_input <- "AI7310"
 
 # me salteo los meses duros de pandemia, pero llego hasta 201907 en training
 # entreno en 18 meses
