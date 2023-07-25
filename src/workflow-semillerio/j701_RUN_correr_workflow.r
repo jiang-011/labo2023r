d27815caae101f59a19bee465e5bbd12389eea4d
jiang-011 <jwmamanilopez@mail.austral.edu.ar>
2023-07-25 10:26:57 -0300

    exp lab sem 1 y 2 alternativo-cantidad de semillas 20

--- src/workflow-semillerio/j701_RUN_correr_workflow.r
+++ src/workflow-semillerio/j701_RUN_correr_workflow.r
@@ -8,6 +8,6 @@ options(error = function() {
 
 
 # corrida de cada paso del workflow
-source("~/labo2023r/src/workflow-semillerio/j741_TS_training_strategy.r")
-source("~/labo2023r/src/workflow-semillerio/j751_HT_lightgbm.r")
+#source("~/labo2023r/src/workflow-semillerio/j741_TS_training_strategy.r")
+#source("~/labo2023r/src/workflow-semillerio/j751_HT_lightgbm.r")
 source("~/labo2023r/src/workflow-semillerio/j771_ZZ_final_semillerio.r")
